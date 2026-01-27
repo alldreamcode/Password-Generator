a351c3dbc2a2b6f4279fd37e04abb4258200255b
alldreamcode <dreamazecreation@gmail.com>
2026-01-26 20:19:52 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let password = "";
+let button1 = document.getElementById("filler1")
+let globalPassword1 = "";
+
+
+function createPassword() {
+    const buttonAddress = [document.getElementById("filler1"), document.getElementById("filler2")];
+    
+    for (let x = 0; x < 2; x++) {
+        
+        for (let i = 0; i < 15; i++) {
+            password += characters[Math.floor((Math.random()*(characters.length)))];
+            
+        }
+        buttonAddress[x].innerText = password; 
+        password = "";
+    }
+    
+}
+
+
+// function copyText() {
+    
+//     await navigator.clipboard.writeText(textToCopy);
+// }
+
+
